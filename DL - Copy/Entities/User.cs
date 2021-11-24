31fbe929b252126b4fc7f9e1869e311e937b4753
ccwilki1 <ccwilki1@gmail.com>
2021-11-24 00:09:13 -0500

    freedom

---
+++ DL - Copy/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DL.Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            Forums = new HashSet<Forum>();
+            Invites = new HashSet<Invite>();
+            Posts = new HashSet<Post>();
+            Replies = new HashSet<Reply>();
+        }
+
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+
+        public virtual ICollection<Forum> Forums { get; set; }
+        public virtual ICollection<Invite> Invites { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Reply> Replies { get; set; }
+    }
+}
---
+++ DL - Copy/UserCloudRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Entity = DL.Entities;
+using Model = Models;
+
+namespace DL
+{
+    public class UserCloudRepo : IUserRepo
+    {
+        private Entity.P3ApiContext _context;
+
+        public UserCloudRepo(Entity.P3ApiContext p_context)
+        {
+            _context = p_context;
+        }
+        public Model.Users AddUser(Model.Users p_Users)
+        {
+            _context.Users.Add
+            (
+                new Entity.User()
+                {
+                    UserName = p_Users.UserName,
+                    UserPass = p_Users.UserPass,
+                    Email = p_Users.Email
+                }
+            );
+            _context.SaveChanges();
+            return p_Users;
+        }
+
+        public List<Model.Users> GetAllUsers()
+        {
+            return _context.Users.Select(user =>
+                new Model.Users()
+                {
+                    ID = user.Id,
+                    UserName = user.UserName,
+                    UserPass = user.UserPass,
+                    Email = user.Email
+                }
+            ).ToList();
+            
+        }
+    }
+}
